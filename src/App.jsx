bb092597a3ac585492fc0aca543a2d4b0aa0c55a
Ryan Moody <ryan@moodyengineer.com>
2023-02-07 19:09:25 -0600

    Install prettier-plugin-tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <main className="min-h-screen">
-      <h1 className="font-bold text-3xl">moodyengineer.com</h1>
+      <h1 className="text-3xl font-bold">moodyengineer.com</h1>
     </main>
   );
 };
